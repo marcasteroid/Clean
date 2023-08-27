e7c328f504643485e1cb1ef31545f9aca782f671
Marco Margarucci <margarucci.marco@gmail.com>
2023-08-27 18:28:41 +0200

    Initial Commit

---
+++ Clean/Clean.h
@@ -0,0 +1,18 @@
+//
+//  Clean.h
+//  Clean
+//
+//  Created by Marco Margarucci on 27/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Clean.
+FOUNDATION_EXPORT double CleanVersionNumber;
+
+//! Project version string for Clean.
+FOUNDATION_EXPORT const unsigned char CleanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Clean/PublicHeader.h>
+
+
