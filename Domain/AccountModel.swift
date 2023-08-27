c63fa41e239ac865c9812aef80696812a56120b2
Marco Margarucci <margarucci.marco@gmail.com>
2023-08-27 18:45:37 +0200

    Added AddAccount use case

---
+++ Domain/AccountModel.swift
@@ -0,0 +1,15 @@
+//
+//  AccountModel.swift
+//  Domain
+//
+//  Created by Marco Margarucci on 27/08/23.
+//
+
+import Foundation
+
+struct AccountModel {
+    var id: String
+    var name: String
+    var email: String
+    var password: String
+}
---
+++ Domain/AddAccount.swift
@@ -0,0 +1,19 @@
+//
+//  AddAccount.swift
+//  Domain
+//
+//  Created by Marco Margarucci on 27/08/23.
+//
+
+import Foundation
+
+protocol AddAccount {
+    func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
+}
+
+struct AddAccountModel {
+    var name: String
+    var email: String
+    var password: String
+    var passwordConfirmation: String
+}
