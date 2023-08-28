bf76cf9277e40dcd586c66055970ebc80ede603d
Marco Margarucci <margarucci.marco@gmail.com>
2023-08-28 20:22:05 +0200

    Move duplicated code to a helper method

--- DataTests/UseCases/RemoteAddAccountTests.swift
+++ DataTests/UseCases/RemoteAddAccountTests.swift
@@ -26,51 +26,27 @@ final class RemoteAddAccountTests: XCTestCase {
     
     func test_add_should_complete_with_error_if_client_completes_with_error() {
         let (sut, httpClientSpy) = makeSut()
-        let expectation = expectation(description: "waiting")
-        sut.add(addAccountModel: makeAddAccountModel()) { result in
-            switch result {
-                case .failure(let error):
-                    XCTAssertEqual(error, .unexpected)
-                case .success:
-                    XCTFail("Expected error, received \(result) instead")
-            }
-            expectation.fulfill()
-        }
-        httpClientSpy.completeWithError(.noConnectivity)
-        wait(for: [expectation], timeout: 1)
+        
+        expect(sut, completeWith: .failure(.unexpected), when: {
+            httpClientSpy.completeWithError(.noConnectivity)
+        })
     }
     
     func test_add_should_complete_with_account_if_client_completes_with_valid_data() {
         let (sut, httpClientSpy) = makeSut()
-        let expectation = expectation(description: "waiting")
-        let expectedAccount = makeAccountModel()
-        sut.add(addAccountModel: makeAddAccountModel()) { result in
-            switch result {
-                case .failure:
-                    XCTFail("Expected success, received \(result) instead")
-                case .success(let receivedAccount):
-                    XCTAssertEqual(receivedAccount, expectedAccount)
-            }
-            expectation.fulfill()
-        }
-        httpClientSpy.completeWithData(expectedAccount.toData()!)
-        wait(for: [expectation], timeout: 1)
+        let account = makeAccountModel()
+        
+        expect(sut, completeWith: .success(account), when: {
+            httpClientSpy.completeWithData(account.toData()!)
+        })
     }
     
     func test_add_should_complete_with_error_if_client_completes_with_invalid_data() {
         let (sut, httpClientSpy) = makeSut()
-        let expectation = expectation(description: "waiting")
-        sut.add(addAccountModel: makeAddAccountModel()) { result in
-            switch result {
-                case .failure(let error):
-                    XCTAssertEqual(error, .unexpected)
-                case .success:
-                    XCTFail("Expected error, received \(result) instead")
-            }
-            expectation.fulfill()
-        }
-        httpClientSpy.completeWithData(Data("invalidData".utf8))
-        wait(for: [expectation], timeout: 1)
+        
+        expect(sut, completeWith: .failure(.unexpected), when: {
+            httpClientSpy.completeWithData(Data("invalidData".utf8))
+        })
     }
 }
 
@@ -87,6 +63,23 @@ extension RemoteAddAccountTests {
         let httpClientSpy = HttpClientSpy()
         let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
         return (sut, httpClientSpy)
+    }
+    
+    func expect(_ sut: RemoteAddAccount, completeWith expectedResult: Result<AccountModel, DomainError>, when action: () -> Void) {
+        let expectation = expectation(description: "waiting")
+        sut.add(addAccountModel: makeAddAccountModel()) { receivedResult in
+            switch (expectedResult, receivedResult) {
+                case (.failure(let expectedError), .failure(let receivedError)):
+                    XCTAssertEqual(expectedError, receivedError)
+                case (.success(let expectedAccount), .success(let receivedAccount)):
+                    XCTAssertEqual(expectedAccount, receivedAccount)
+                default:
+                    XCTFail("Expected \(expectedResult), received \(receivedResult) instead")
+            }
+            expectation.fulfill()
+        }
+        action()
+        wait(for: [expectation], timeout: 1)
     }
     
     class HttpClientSpy: HttpPostClient {
