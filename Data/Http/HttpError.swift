7c76b8ceb12b327634233fd4bc3b3e43a41fc105
Marco Margarucci <margarucci.marco@gmail.com>
2023-08-28 16:53:05 +0200

    Ensure RemoteAddAccount completes with error when HttpClient completes with error

---
+++ Data/Http/HttpError.swift
@@ -0,0 +1,12 @@
+//
+//  HttpError.swift
+//  Data
+//
+//  Created by Marco Margarucci on 28/08/23.
+//
+
+import Foundation
+
+public enum HttpError: Error {
+    case noConnectivity
+}
---
+++ Domain/Errors/DomainError.swift
@@ -0,0 +1,12 @@
+//
+//  DomainError.swift
+//  Domain
+//
+//  Created by Marco Margarucci on 28/08/23.
+//
+
+import Foundation
+
+public enum DomainError: Error {
+    case unexpected
+}
