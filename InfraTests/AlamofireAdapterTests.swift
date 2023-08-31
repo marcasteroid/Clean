95d4e504b5728a69b2955ff1414592a88598610a
Marco Margarucci <margarucci.marco@gmail.com>
2023-08-31 09:06:15 +0200

    Added AlamoFireAdapterTests scaffold

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  AlamofireAdapterTests.swift.swift
+//  AlamofireAdapterTests.swift
+//
+//  Created by Marco Margarucci on 31/08/23.
+//
+
+import XCTest
+
+final class InfraTests: XCTestCase {
+    
+    func test_() {
+        
+    }
+}
