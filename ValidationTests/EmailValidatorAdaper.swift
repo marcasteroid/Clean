607de3be490ff9a90336369891324b1c3381a681
Marco Margarucci <margarucci.marco@gmail.com>
2024-01-13 13:36:54 +0100

    Add Validation target

---
+++ ValidationTests/EmailValidatorAdaper.swift
@@ -0,0 +1,14 @@
+//
+//  EmailValidatorAdaper.swift
+//  EmailValidatorAdaper
+//
+//  Created by Marco Margarucci on 13/01/24.
+//
+
+import XCTest
+
+final class ValidationTests: XCTestCase {
+
+    func test_()  {
+    }
+}
